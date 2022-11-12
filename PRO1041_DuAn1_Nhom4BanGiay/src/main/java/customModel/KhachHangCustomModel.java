13726f27e4b059e241e3a46ddca3582dc221ba64
Ngô Vũ Ngọc Hào <ngovungochao14112003@gmail.com>
2022-11-12 22:32:11 +0700

    curdKhachHang

---
+++ PRO1041_DuAn1_Nhom4BanGiay/src/main/java/customModel/KhachHangCustomModel.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customModel;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class KhachHangCustomModel {
+
+    private String id;
+    private String ma;
+    private String hoTen;
+    private String sdt;
+
+    public KhachHangCustomModel() {
+    }
+
+    public KhachHangCustomModel(String id, String ma, String hoTen, String sdt) {
+        this.id = id;
+        this.ma = ma;
+        this.hoTen = hoTen;
+        this.sdt = sdt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    @Override
+    public String toString() {
+        return "KhachHang{" + "id=" + id + ", ma=" + ma + ", hoTen=" + hoTen + ", sdt=" + sdt + '}';
+    }
+}
